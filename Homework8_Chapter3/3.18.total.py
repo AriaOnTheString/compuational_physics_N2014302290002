4fa436aa0efd37f8e912b8fa7cee52bf779ae345
Yijun He <751885712@qq.com>
2016-11-13 18:06:27 +0800

    Update 3.18.total.py

--- Homework8_Chapter3/3.18.total.py
+++ Homework8_Chapter3/3.18.total.py
@@ -36,68 +36,28 @@ class pendulums():
         pl.xlabel('Time ($s$)')
         pl.ylabel('Angular velocity ($rad/s$)')
        
-pl.subplot(2,2,1)
+pl.subplot(3,1,1)
 a=pendulums(9.8, 3*math.pi/1000)
-a.calculation(0.5,0.2,0,1.4,2/3, 3*math.pi*2000)
-showangle=[]
-showomega=[]
-for i in range(10000,len(a.t)):
-        if ((2/3)*(a.t[i]))%(2*(math.pi))<0.001 :
-            showangle.append(a.angle[i])
-            showomega.append(a.omega[i])
-pl.plot(showangle, showomega,'.',label='$2/3t=2\pi n$')
-pl.xlabel('Angle ($rad$)')
-pl.ylabel('Angular ($rad/s$)')
-pl.xlim(-4,4)
-pl.ylim(-2,2)
+a.calculation(0.5,0.2,0,1.4,2/3, math.pi*30)
+pl.plot(a.t, a.angle,label='$F_D=1.4$')
+pl.xlabel('Time ($s$)')
+pl.ylabel('Angle ($rad$)')
 pl.legend(loc='best')
 
-pl.subplot(2,2,2)
+pl.subplot(3,1,2)
 a=pendulums(9.8, 3*math.pi/1000)
-a.calculation(0.5,0.2,0,1.4,2/3,3*math.pi*2000)
-showangle=[]
-showomega=[]
-for i in range(10000,len(a.t)):
-        if ((2/3)*(a.t[i])-math.pi/4)%(2*(math.pi))<0.001 :
-            showangle.append(a.angle[i])
-            showomega.append(a.omega[i])
-pl.plot(showangle, showomega,'.',label='$2/3t=2\pi n+\pi/4$')
-pl.xlabel('Angle ($rad$)')
-pl.ylabel('Angular ($rad/s$)')
-pl.xlim(-4,4)
-pl.ylim(-2,2)
+a.calculation(0.5,0.2,0,1.44,2/3,math.pi*30)
+pl.plot(a.t, a.angle,label='$F_D=1.44$')
+pl.xlabel('Time ($s$)')
+pl.ylabel('Angle ($rad$)')
 pl.legend(loc='best')
 
-pl.subplot(2,2,3)
+pl.subplot(3,1,3)
 a=pendulums(9.8, 3*math.pi/1000)
-a.calculation(0.5,0.2,0,1.4,2/3,3*math.pi*2000)
-showangle=[]
-showomega=[]
-for i in range(10000,len(a.t)):
-        if ((2/3)*(a.t[i])-math.pi/2)%(2*(math.pi))<0.001 :
-            showangle.append(a.angle[i])
-            showomega.append(a.omega[i])
-pl.plot(showangle, showomega,'.',label='$2/3t=2\pi n+\pi/2$')
-pl.xlabel('Angle ($rad$)')
-pl.ylabel('Angular ($rad/s$)')
-pl.xlim(-4,4)
-pl.ylim(-2,2)
-pl.legend(loc='best')
-
-pl.subplot(2,2,4)
-a=pendulums(9.8, 3*math.pi/1000)
-a.calculation(0.5,0.2,0,1.4,2/3,3*math.pi*2000)
-showangle=[]
-showomega=[]
-for i in range(10000,len(a.t)):
-        if ((2/3)*(a.t[i])-3*math.pi/4)%(2*(math.pi))<0.001 :
-            showangle.append(a.angle[i])
-            showomega.append(a.omega[i])
-pl.plot(showangle, showomega,'.',label='$2/3t=2\pi n+3\pi/4$')
-pl.xlabel('Angle ($rad$)')
-pl.ylabel('Angular ($rad/s$)')
-pl.xlim(-4,4)
-pl.ylim(-2,2)
+a.calculation(0.5,0.2,0,1.465,2/3,math.pi*30)
+pl.plot(a.t, a.angle,label='$F_D=1.465$')
+pl.xlabel('Time ($s$)')
+pl.ylabel('Angle ($rad$)')
 pl.legend(loc='best')
 
 pl.show()   
